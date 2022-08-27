566fafe97e26f35758b34ed87dc62adb0863877b
Pankaj Sharma <sharmapankaj2512@gmail.com>
2022-08-27 14:03:38 +0400

    move class to separate file

---
+++ lib/Participant.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Participant extends StatelessWidget {
+  final String participant;
+
+  const Participant({Key? key, required this.participant}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+        widthFactor: 0.7,
+        heightFactor: 0.3,
+        child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.blue,
+              ),
+              borderRadius: const BorderRadius.all(Radius.circular(20)),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.blue.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: const Offset(0, 3), // changes position of shadow
+                ),
+              ],
+            ),
+            child: Center(
+              child: Text(
+                participant,
+                maxLines: 1,
+                softWrap: true,
+                overflow: TextOverflow.fade,
+              ),
+            )));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:mobmaid/mob.dart';
 
+import 'Participant.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -92,41 +94,5 @@ class LandingPageState extends State<LandingPage> {
     setState(() {
       _mob.join(participant);
     });
-  }
-}
-
-class Participant extends StatelessWidget {
-  final String participant;
-
-  const Participant({Key? key, required this.participant}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return FractionallySizedBox(
-        widthFactor: 0.7,
-        heightFactor: 0.3,
-        child: Container(
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Colors.blue,
-              ),
-              borderRadius: const BorderRadius.all(Radius.circular(20)),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.blue.withOpacity(0.5),
-                  spreadRadius: 5,
-                  blurRadius: 7,
-                  offset: const Offset(0, 3), // changes position of shadow
-                ),
-              ],
-            ),
-            child: Center(
-              child: Text(
-                participant,
-                maxLines: 1,
-                softWrap: true,
-                overflow: TextOverflow.fade,
-              ),
-            )));
   }
 }
