f464ff81dac310837ff91f0d16f8da17cf2ff455
Pankaj Sharma <sharmapankaj2512@gmail.com>
2022-08-27 14:02:02 +0400

    extract stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -78,32 +78,7 @@ class LandingPageState extends State<LandingPage> {
                         ),
                         children:
                             _mob.participants().map<Widget>((participant) {
-                          return FractionallySizedBox(
-                              widthFactor: 0.7,
-                              heightFactor: 0.3,
-                              child: Container(
-                                  decoration: BoxDecoration(
-                                    border: Border.all(
-                                      color: Colors.blue,
-                                    ),
-                                    borderRadius: const BorderRadius.all(Radius.circular(20)),
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color: Colors.blue.withOpacity(0.5),
-                                        spreadRadius: 5,
-                                        blurRadius: 7,
-                                        offset: const Offset(0, 3), // changes position of shadow
-                                      ),
-                                    ],
-                                  ),
-                                  child: Center(
-                                    child: Text(
-                                      participant,
-                                      maxLines: 1,
-                                      softWrap: true,
-                                      overflow: TextOverflow.fade,
-                                    ),
-                                  )));
+                          return Participant(participant: participant);
                         }).toList()))
               ],
             ),
@@ -117,5 +92,41 @@ class LandingPageState extends State<LandingPage> {
     setState(() {
       _mob.join(participant);
     });
+  }
+}
+
+class Participant extends StatelessWidget {
+  final String participant;
+
+  const Participant({Key? key, required this.participant}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+        widthFactor: 0.7,
+        heightFactor: 0.3,
+        child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.blue,
+              ),
+              borderRadius: const BorderRadius.all(Radius.circular(20)),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.blue.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: const Offset(0, 3), // changes position of shadow
+                ),
+              ],
+            ),
+            child: Center(
+              child: Text(
+                participant,
+                maxLines: 1,
+                softWrap: true,
+                overflow: TextOverflow.fade,
+              ),
+            )));
   }
 }
